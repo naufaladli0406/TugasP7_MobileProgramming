a263183888a5ab432988c8c214334e18391d2731
Muhammad Naufal Adli <naufaladli0406@gmail.com>
2025-06-15 00:20:14 +0700

    Initial Commit

---
+++ NaufalP7/NaufalP7App.swift
@@ -0,0 +1,17 @@
+//
+//  NaufalP7App.swift
+//  NaufalP7
+//
+//  Created by Naufal Adli on 15/06/25.
+//
+
+import SwiftUI
+
+@main
+struct NaufalP7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
